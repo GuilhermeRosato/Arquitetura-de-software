d8a6667992d21d83d87a2ef360ea0f789e2cefef
Guilherme Rosato Novaes de Souza <guigui.rosato@gmail.com>
2025-03-30 19:41:03 -0300

    Update Main.java

--- aula2/Main.java
+++ aula2/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
 public static void main(String[] args) {
 //System.out.println("Hello World");
-Narguile narguile1 = new Narguile();
+Narguile narguile1 = new Narguile("aluminio", "tutti fruti", "coco",1);
 narguile1.setQtdCarvao(2);
 narguile1.setRosh("Ceramica");
 System.out.println("Rosh: "+narguile1.getRosh());
