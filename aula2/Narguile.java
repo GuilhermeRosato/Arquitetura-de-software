2610edc94350119d109c90107a32495e04997766
Guilherme Rosato Novaes de Souza <guigui.rosato@gmail.com>
2025-03-30 19:39:25 -0300

    Create Narguile.java

---
+++ aula2/Narguile.java
@@ -0,0 +1,31 @@
+public class Narguile {
+        private String rosh = "aluminio";
+        private String essencia = "tutti frutti";
+        private String tpCarvao = "coco";
+        private int qtdCarvao = 1;
+        // get e set
+        public String getRosh(){
+            return this.rosh;
+        }
+        public int getQtdCarvao(){
+            return this.qtdCarvao;
+        }
+        public String getEssencia(){
+            return this.essencia;
+        }
+        public String getTpCarvao(){
+            return this.tpCarvao;
+        }
+        public void setRosh(String rosh){
+            this.rosh = rosh;
+        }
+        public void setTpCarvao(String tpCarvao){
+            this.tpCarvao = tpCarvao;
+        }
+        public void setEssencia(String essencia){
+            this.essencia = essencia;
+        }
+        public void setQtdCarvao(int qtdCarvao){
+            this.qtdCarvao = qtdCarvao;
+        }
+}
