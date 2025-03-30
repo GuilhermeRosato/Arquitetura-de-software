8b914aaac2c5355a7675d45d18b930fca4cf0c58
Guilherme Rosato Novaes de Souza <guigui.rosato@gmail.com>
2025-03-30 19:44:28 -0300

    Create Usuario.java

---
+++ aula4/Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+    private String nome;
+
+    public Usuario() {//inicializar objeto instanciado
+        this.nome = "";
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
