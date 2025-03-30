9711bbb83e117f055c43926e2c23c3e7f711fb47
Guilherme Rosato Novaes de Souza <guigui.rosato@gmail.com>
2025-03-30 19:39:03 -0300

    Create Main.java

---
+++ aula2/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+public static void main(String[] args) {
+//System.out.println("Hello World");
+Narguile narguile1 = new Narguile();
+narguile1.setQtdCarvao(2);
+narguile1.setRosh("Ceramica");
+System.out.println("Rosh: "+narguile1.getRosh());
+System.out.println("Essencia: "+narguile1.getEssencia());
+System.out.println("Tipo do carvão: "+narguile1.getTpCarvao());
+System.out.println("Quantidade de carvão: "+narguile1.getQtdCarvao());
+}
+}
