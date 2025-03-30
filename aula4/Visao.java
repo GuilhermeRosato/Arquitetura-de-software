3c44a7a569f1c8a31e6ddb371835696b2c62b4ad
Guilherme Rosato Novaes de Souza <guigui.rosato@gmail.com>
2025-03-30 19:44:50 -0300

    Create Visao.java

---
+++ aula4/Visao.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Visao {
+    private Scanner scanner;
+
+    public Visao() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String obterNome() {
+        System.out.print("Digite seu nome: ");
+        return scanner.nextLine();
+    }
+
+    public void mostrarMensagem(String mensagem) {
+        System.out.println(mensagem);
+    }
+}
